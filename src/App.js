04ff62bed1e52c0b7f142e2090d2063aaab4ae27
Ivan Lee <ilee.code@gmail.com>
2020-09-29 16:51:40 -0700

    Implemented outline of app component with button, input field, and list.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <input></input>
+      <button>Add Todo</button>
+      <ul>
+        <li>Take dogs for a walk</li>
+        <li>Take trash out</li>
+      </ul>
     </div>
   );
 }
