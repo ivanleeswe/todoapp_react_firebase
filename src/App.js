e8de441f3bc997e51affb5d783cb55e2b562b93c
Ivan Lee <ilee.code@gmail.com>
2020-09-29 17:24:30 -0700

    Implemented state for todos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <h1>hello world</h1>
       <input></input>
       <button>Add Todo</button>
+
       <ul>
         <li>Take dogs for a walk</li>
         <li>Take trash out</li>
       </ul>
+
     </div>
   );
 }
