bf383eae07bc3b9a8cf6c48ca01d06dc87aa4916
Ivan Lee <ilee.code@gmail.com>
2020-09-29 23:38:20 -0700

    Implemented changes to input

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take trash out']);
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take trash out', 'Jump rope']);
+  const [input, setInput] = useState('');
 
   return (
     <div className="App">
-      <h1>hello world</h1>
-      <input></input>
+      <h1>hello world!</h1>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
       <button>Add Todo</button>
 
       <ul>
