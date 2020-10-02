cd9fc7077d3a7cb353dfaa349e70b674737bc349
Ivan Lee <ilee.code@gmail.com>
2020-10-02 15:47:23 -0700

    Implemented modal

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { List, ListItem, ListItemText, ListItemAvatar, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { List, ListItem, ListItemText, ListItemAvatar, Modal } from '@material-ui/core';
 import db from './firebase';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Todo(props) {
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () =>{
+    setOpen(true);
+  }
   return (
+    <>
+    <Modal
+      open={open}
+      onClose={e => setOpen(false)}
+    >
+      <div>
+        <h1>i am a modal</h1>
+        <button onClick={e => setOpen(false)}></button>
+      </div>
+    </Modal>
 
     <List>
       <ListItem>
@@ -12,9 +28,10 @@ function Todo(props) {
         </ListItemAvatar>
         <ListItemText primary={props.todo.todo} secondary="Dummy deadline" />
       </ListItem>
+      <button onClick={e => setOpen(true)}>Edit</button>
       <DeleteForeverIcon onClick={event => { db.collection('todos').doc(props.todo.id).delete()} } />
     </List>
-
+    </>
   )
 }
 
