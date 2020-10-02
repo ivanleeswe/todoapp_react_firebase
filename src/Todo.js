34c59754651122f653e837fa19ce685c3a9c1934
Ivan Lee <ilee.code@gmail.com>
2020-10-01 21:09:30 -0700

    Changed delete button to icon using material ui icons

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { List, ListItem, ListItemText, ListItemAvatar, Button } from '@material-ui/core';
 import db from './firebase';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Todo(props) {
   return (
 
     <List>
       <ListItem>
-        <ListItemAvatar>
+        <ListItemAvatar> 
         </ListItemAvatar>
         <ListItemText primary={props.todo.todo} secondary="Dummy deadline" />
       </ListItem>
-      <Button onClick={event => {
-        db.collection('todos').doc(props.todo.id).delete()}}>Delete Me</Button>
+      <DeleteForeverIcon onClick={event => { db.collection('todos').doc(props.todo.id).delete()} } />
     </List>
 
   )
